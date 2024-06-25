1561d93a754d39b73afebac9da1604d8bacfbae8
luizafmorgado <luizafernandaa@gmail.com>
2024-06-24 21:30:31 -0300

    mensagem

---
+++ date_time.py
@@ -0,0 +1,13 @@
+from datetime import date, datetime, time
+
+data = date(2024, 6, 24)
+print(data)
+
+print(date.today())
+
+data_hora = datetime(2024, 6, 24,14, 36, 10)
+print(data_hora)
+print(datetime.today())
+
+hora = time(10, 20, 0)
+print(hora)
